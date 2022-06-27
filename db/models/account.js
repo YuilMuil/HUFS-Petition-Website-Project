bcbe70de458b2d883e47dcca7e52f637aafacf92
YuilMuil <6235519+YuilMuil@users.noreply.github.com>
2022-06-27 23:46:24 +0900

    Account 데이터베이스 모델 업데이트
    Ban_Reason 사유를 enum으로 하드코딩을 하고 enum번호는 데이터베이스에서 저장/가져오기!

--- db/models/account.js
+++ db/models/account.js
@@ -10,9 +10,9 @@ var accountSchema = new Schema(
         password: String,
         SchoolID: Number,
         RegistrationDate: Date,
-        AccountRank: {type: Number, default: 0} //1->운영자 0->일반 유저 -1->특별한 사유때문에 계정 정지 상태
-        //Ban_Reason: {type: string||short, default: NULL} //옆에 pseudo-code처럼 정지를 할 경우 사유도 표시되도록 만들수있다. 이거를 enum으로 할테지 아니면 그냥 database에서 string으로 할지는 팀의 결정이다..
-    }
+        AccountRank: {type: Number, default: 0}, //1->운영자 0->일반 유저 -1->특별한 사유때문에 계정 정지 상태
+        Ban_Reason: {type: Number, default: 0} //옆에 pseudo-code처럼 정지를 할 경우 사유도 표시되도록 만들수있다. 이거를 enum으로 할테지 아니면 그냥 database에서 string으로 할지는 팀의 결정이다..
+   }
 )
 
 var accountModel = mongoose.model('account', accountSchema);
